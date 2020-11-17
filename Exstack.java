58be7d11cf2481d07271ba7da80e7ff4ea95e6b2
hajarhesham <47723308+hajarhesham@users.noreply.github.com>
2020-11-17 03:24:03 +0200

    Add files via upload

---
+++ Exstack.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exstack;
+
+/**
+ *
+ * @author Hajar
+ */
+public class Exstack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        stack st = new stack();
+        st.push1(10);
+        st.push1(6);
+        st.push1(3);
+    }
+    
+}
---
+++ stack.java
@@ -0,0 +1,46 @@
+
+package exstack;
+public class stack {
+    private class Node{
+        private int value;
+        private Node next;
+        
+    public Node(int value){
+        this.value = value;
+    }  
+    }
+    
+    private Node first;
+    private Node last;
+    
+    public void push(int value){
+       var newlink = new Node(value);
+        if(first == null){
+            first = newlink;    
+        }
+        newlink.next=first;
+        first = newlink;
+
+    }
+    public int peek(){
+        return first.value;
+    
+    }
+    public void pop(){
+        first = first.next;
+    }
+    public void push1(int value){
+      var newlink = new Node(value);
+        if(first ==null)first =newlink;
+        else
+            last.next=newlink;
+  
+        last =newlink;}
+                
+    
+    
+    
+    
+            
+    
+}
